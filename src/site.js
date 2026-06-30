5813f3678dd7189d4da9538ca6b3e91f992d73cf
Joelle Voogt <j.voogt@youweagency.com>
2026-06-30 11:41:11 +0200

    Changed dark mode logo

--- src/site.js
+++ src/site.js
@@ -1,6 +1,7 @@
 require('./common')
 require('./images/logo.png')
 require('./images/radar_legend.png')
+require('./images/youwe-logo-dark.png')
 require('./analytics.js')
 
 const Factory = require('./util/factory')
